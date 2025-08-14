286a3ffb71898af692da8f77a9195bdbd3343f34
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-15 00:58:09 +0900

    wip

--- manage/bin/params.sh
+++ manage/bin/params.sh
@@ -1,3 +1,3 @@
 cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envtry.txt
-pthdoc="${pthtop}"/export/cnf/xoxxox/docker_envtry
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envtry
 imgtgt='xoxxox/envtry:001'
