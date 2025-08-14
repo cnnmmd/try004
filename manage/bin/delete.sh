db10aaaae01f2ee4fcb0bfbe2d9955a6d855f8ca
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-15 00:34:18 +0900

    wip

---
+++ manage/bin/delete.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pthtop="$(cd "$(dirname "${0}")/../../../.." && pwd)"
+source "${pthtop}"/manage/lib/params.sh
+source "${pthtop}"/manage/lib/shared.sh
+source "${pthcrr}"/params.sh
+
+delimg ${imgtgt}
